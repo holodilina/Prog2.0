1455cdb8cdaa94a73839a725e97e4b7243a2d790
Natalya <holodilina.n@gmail.com>
2022-09-13 18:26:05 +0300

    открытие для просмотра

---
+++ Test1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Тест программа того, что в числе нет заданной цифры
+Console.WriteLine("Введите число: ");
+
+int main()
+{
+    int x,a;
+    bool exixt=false;
+    cout<<"Enter x=";
+    
+
+}
