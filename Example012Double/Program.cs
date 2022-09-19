300afb653e0e6ee098a84c8afda9f7d50973140e
Natalya <holodilina.n@gmail.com>
2022-09-19 17:10:55 +0300

    добавление к примеру 012 второго варианта

--- Example012Double/Program.cs
+++ Example012Double/Program.cs
@@ -25,4 +25,10 @@ int max2 = Max(a2, b2, c2);
 int max3 = Max(a3, b3, c3);
 int max = Max(max1, max2, max3);
 
+// можно код из строк 23-26 записать в одной строке:
+// int max = Max(
+//    Max(a1, b1, c1),
+//    Max(a2, b2, c2),
+//    Max(a3, b3, c3));
+
 Console.WriteLine(max);
