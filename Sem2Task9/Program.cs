0512ef6df08effaa7c09326c5353fbf9f997b0a1
Natalya <holodilina.n@gmail.com>
2022-09-19 18:37:42 +0300

    изменения в Sem2Task9

---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,19 @@
+﻿// #9 Программа, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+System.Random numberGenerator = new System.Random();
+
+int number = numberGenerator.Next(10, 100);
+
+Console.WriteLine(number);
+
+int firstDigit = number/10;
+int secondDigit = number%10;
+
+if(firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
