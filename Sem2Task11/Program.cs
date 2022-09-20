988845dc8b71c94505baffddfbdc1d468eb476d3
Natalya <holodilina.n@gmail.com>
2022-09-20 16:48:01 +0300

    2\11

--- Sem2Task11/Program.cs
+++ Sem2Task11/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Ввести трёхзначное число и удалить у него вторую цифру,
 // вывести пользователю число, которое содержит первую и третью цифру.
 
-Console.WriteLine(number);
+// Console.WriteLine(number);
 
-int firstDigit = number / 100;
-int thirdDigit = number % 10;
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
 
 //Вариант Евгения
 void Variant2()
@@ -31,9 +31,16 @@ void Variant1()
 }
 
 // Вариант Михаила
-
-
-
+void Variant3()
+{
+    Console.WriteLine("Метод 3");
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(100, 1000);
+    Console.WriteLine(number);
+    char [] charArray = number.ToString().ToCharArray();
+    Console.WriteLine("" + charArray[0] + charArray[2]);
+}
 
 Variant2();
 Variant1();
+Variant3();
