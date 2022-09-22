243dad318104b4f21e11a901cb3ab5a96a0c9df8
Natalya <holodilina.n@gmail.com>
2022-09-22 13:38:48 +0300

    Add changes to folder 3Sem3Task18

---
+++ 3Sem3Task20/Program.cs
@@ -0,0 +1,31 @@
+﻿// №20 
+
+int ReadData(string line) 
+{    
+    Console.WriteLine(line); 
+    int number = int.Parse(Console.ReadLine() ?? "0"); 
+    return number;
+}
+
+string QuterBorderAsk(int numQuter)
+{
+    if (numQuter == 1)
+    return "x > 0, y > 0";
+    if (numQuter == 2)
+    return "x < 0, y > 0";
+    if (numQuter == 3)
+    return "x < 0, y < 0";
+    if (numQuter == 4)
+    return "x > 0, y < 0";
+    
+    return "---";
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int num = ReadData("Введите номер четверти: ");
+string res = QuterBorderAsk(num);
+PrintResult(res);
