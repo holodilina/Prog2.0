cd79c899d1e4d415d4555bb32df53357a59047fa
Natalya <holodilina.n@gmail.com>
2022-09-22 15:07:36 +0300

    Add all to Task20

---
+++ 3Sem3Task20.1/Program.cs
@@ -0,0 +1,28 @@
+﻿/// №20.1 Вариант 2. Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D-пространстве
+
+int ReadData(string line) 
+{    
+    Console.WriteLine(line); 
+    int number = int.Parse(Console.ReadLine() ?? "0"); 
+    return number;
+}
+
+// Расстояние между двумя точками
+double CalculateDistance(int x1, int y1, int x2, int y2)
+{
+    double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    return distance;
+}
+    int x1 = ReadData("Введите координату X первой точки: ");
+    int x2 = ReadData("Введите координату Y первой точки: ");
+    int y1 = ReadData("Введите координату X второй точки: ");
+    int y2 = ReadData("Введите координату Y второй точки: ");
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+double result = CalculateDistance(x1, y1, x2, y2);
+PrintResult("Расстояние между двумя точками: " + Math.Round(result, 2));
---
+++ 3Sem3Task22/Program.cs
@@ -0,0 +1,50 @@
+﻿// №22 Напишите программу, которая принимает на вход число N
+// и выдает таблицу квадратов чисел от 1 до N
+
+int ReadData(string line) 
+{    
+    Console.WriteLine(line); 
+    int number = int.Parse(Console.ReadLine() ?? "0"); 
+    return number;
+}
+
+// Метод, который собирает верхушку таблицы
+string LineNumbers(int numberN)
+{
+    int i = 1;
+    string outLine = string.Empty;
+    while (i < numberN)
+    {
+        outLine = outLine + i + "\t"; //использовать табуляцию /t , чтобы выровнять как таблицу
+        ++i;
+    }
+    outLine = outLine + numberN;
+    return outLine;
+}
+
+// Метод, который собирает низ таблицы
+string LineSqr(int numberN)
+{
+    int i = 1;
+    string outLine = string.Empty;
+    while (i < numberN)
+    {
+        outLine = outLine + i * i + "\t";
+        ++i;
+    }
+    outLine = outLine + numberN * numberN;
+    return outLine;
+}
+
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int num = ReadData("Введите число N: ");
+string lineTop = LineNumbers(num);
+string lineDown = LineSqr(num);
+
+PrintResult(lineTop);
+PrintResult(lineDown);
