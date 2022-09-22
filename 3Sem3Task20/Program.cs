e99ca23968f0e61a2eaab771c3b3e62a758bf7ed
Natalya <holodilina.n@gmail.com>
2022-09-22 14:17:27 +0300

    Add all for folder 3Sem3Task20

--- 3Sem3Task20/Program.cs
+++ 3Sem3Task20/Program.cs
@@ -1,4 +1,5 @@
-﻿// №20 
+﻿// №20 Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D-пространстве
 
 int ReadData(string line) 
 {    
@@ -7,25 +8,27 @@ int ReadData(string line)
     return number;
 }
 
-string QuterBorderAsk(int numQuter)
+// Расстояние между двумя точками
+double CalcData(int x1, int y1, int x2, int y2)
 {
-    if (numQuter == 1)
-    return "x > 0, y > 0";
-    if (numQuter == 2)
-    return "x < 0, y > 0";
-    if (numQuter == 3)
-    return "x < 0, y < 0";
-    if (numQuter == 4)
-    return "x > 0, y < 0";
-    
-    return "---";
+    double res = 0;
+    res = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    return res;
 }
+
+    int x1 = ReadData("Введите x1 ");
+    int x2 = ReadData("Введите x2 ");
+    int y1 = ReadData("Введите y1 ");
+    int y2 = ReadData("Введите y2 ");
 
 void PrintResult(string line)
 {
     Console.WriteLine(line);
 }
 
-int num = ReadData("Введите номер четверти: ");
-string res = QuterBorderAsk(num);
-PrintResult(res);
+// PrintResult(CalcData(x1, y1, x2, y2).ToString());
+// Math.Round(); // округление значения
+
+// округление значения до какой-то цифры до запятой, например, до 2-й
+PrintResult(Math.Round(CalcData(x1, y1, x2, y2), 2).ToString());
+
