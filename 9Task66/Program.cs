85070543427f1c5c83608cbbce9359507c703327
Natalya <holodilina.n@gmail.com>
2022-09-28 13:59:43 +0300

    добавлено решение задачи 66

---
+++ 9Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N.
+// Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N.
+
+// выводит все числа от M до N
+Console.WriteLine();
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Натуральные числа от N до M:");
+int sum = 0;
+for (int i = M; i <= N; i++)
+{
+    Console.Write($" {i}"); 
+    sum+=i;
+}
+Console.WriteLine();
+
+// находит сумму всех значений от M до N
+Console.WriteLine($"Сумма натуральных элементов в промежутк от M до N: {sum}");
+Console.WriteLine();
